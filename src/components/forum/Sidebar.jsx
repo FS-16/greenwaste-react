7633269e72abcb59d5ccc26a11719d21f94e2256
koreoxy <rayssankn@gmail.com>
2023-11-16 01:19:40 +0700

    fix: page all qeustion and sidebar menu

--- src/components/forum/Sidebar.jsx
+++ src/components/forum/Sidebar.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { IoListCircle } from 'react-icons/io5';
-import { RiQuestionFill, RiAccountCircleFill } from 'react-icons/ri';
-import { SiQuantconnect } from 'react-icons/si';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { IoListCircle } from "react-icons/io5";
+import { RiQuestionFill, RiAccountCircleFill } from "react-icons/ri";
+import { SiQuantconnect } from "react-icons/si";
+import { NavLink } from "react-router-dom";
 
 function Sidebar() {
   const navigation = [
     {
-      name: 'All Question',
-      href: '/forum/all-question',
+      name: "All Question",
+      href: "/forum/all-question",
       icon: (
         <IoListCircle
           size={30}
@@ -19,8 +19,8 @@ function Sidebar() {
       ),
     },
     {
-      name: 'Ask Question',
-      href: '/forum/ask-question',
+      name: "Ask Question",
+      href: "/forum/ask-question",
       icon: (
         <RiQuestionFill
           size={30}
@@ -31,8 +31,8 @@ function Sidebar() {
       ),
     },
     {
-      name: 'My Question',
-      href: '/forum/my-question',
+      name: "My Question",
+      href: "/forum/my-question",
       icon: (
         <RiAccountCircleFill
           size={30}
@@ -43,8 +43,8 @@ function Sidebar() {
       ),
     },
     {
-      name: 'My Participation',
-      href: '/forum/my-participation',
+      name: "My Participation",
+      href: "/forum/my-participation",
       icon: (
         <SiQuantconnect
           size={30}
@@ -58,17 +58,17 @@ function Sidebar() {
 
   return (
     <div>
-      <div className="flex flex-col mt-10 mx-32 border-r-2">
+      <div className="flex flex-col mt-10 border-r-2">
         {/* SIDEBAR */}
-        <div className="pr-20">
+        <div className="pr-16">
           {navigation.map((item) => (
             <NavLink
               to={item.href}
               key={item.name}
               className={({ isActive }) => {
                 return !isActive
-                  ? 'text-black flex flex-row items-center gap-2 p-2 font-semibold text-base'
-                  : 'text-[#1a643d] bg-[#cff3bd] rounded-full flex flex-row items-center gap-2 p-2 font-semibold text-base w-48';
+                  ? "text-gray-600 flex flex-row items-center gap-2 p-2 font-normal text-base hover:bg-[#cff3bd] hover:rounded-full m-2"
+                  : "text-[#1a643d] bg-[#cff3bd] rounded-full flex flex-row items-center gap-2 p-1 font-semibold text-base w-48 border-2 border-green-800 m-2";
               }}
             >
               {item.icon}
--- src/components/forum/pages/AllQuestion.jsx
+++ src/components/forum/pages/AllQuestion.jsx
@@ -1,13 +1,108 @@
-import React from 'react';
-import Sidebar from '../Sidebar';
+import React from "react";
+import Sidebar from "../Sidebar";
 
 function AllQuestion() {
+  const data = [
+    {
+      id: 1,
+      title:
+        "Bagaimana membuat limbah rumah tangga menjadi produk yang berkualitas ?",
+      description:
+        "Apa langkah-langkah yang dapat diambil untuk mengubah limbah elektronik rumah tangga menjadi produk elektronik yang bisa digunakan kembali atau didaur ulang dengan efisien?",
+      category: [
+        {
+          category1: "Limbah organik",
+          category2: "Limbah anorganik",
+          category3: "Limbah B3",
+        },
+      ],
+      username: "Brian",
+      answers: "12 answers",
+    },
+    {
+      id: 2,
+      title: "Peraturan dan kebijakan pemerintah terkait limbah",
+      description:
+        "Bagaimana peraturan dan kebijakan pemerintah dapat membantu dalam mengurangi dampak limbah anorganik dan mempromosikan pengelolaan yang lebih baik?",
+      category: [
+        {
+          category1: "Limbah organik",
+          category2: "Limbah anorganik",
+          category3: "Limbah B3",
+        },
+      ],
+      username: "Jerrrr",
+      answers: "5 answers",
+    },
+    {
+      id: 3,
+      title: "Limbah B3 Meningkat",
+      description:
+        "Bagaimana perusahaan atau organisasi dapat meningkatkan kesadaran dan pelatihan untuk mengelola limbah B3 dengan aman?",
+      category: [
+        {
+          category1: "Limbah organik",
+          category2: "Limbah anorganik",
+          category3: "Limbah B3",
+        },
+      ],
+      username: "ASDSADSA",
+      answers: "10 answers",
+    },
+  ];
+
   return (
-    <div className="container">
-      <div className="flex flex-row">
+    <div className="flex flex-row justify-center">
+      <div className="flex-none">
         <Sidebar />
-        <div className="mt-10 ml-[-20px]">
-          <h1> All Question</h1>
+      </div>
+
+      <div className="flex-initial w-[650px] mt-10">
+        <div className="flex flex-col">
+          <div className="flex flex-row justify-between ml-10 border-b-2 pb-4">
+            <div className="text-xl pt-2 font-bold">
+              <h1>All Questions</h1>
+            </div>
+            <div>
+              <button className="bg-green-500 text-white p-2 text-sm rounded-md hover:bg-green-900">
+                Ask Question
+              </button>
+            </div>
+          </div>
+
+          {/* LIST ALL QUESTION */}
+          {data.map((item) => (
+            <div
+              className="flex flex-col ml-10 mt-10 border-b-2 pb-5"
+              key={item.id}
+            >
+              <div className="flex flex-row justify-between">
+                <div className="w-[490px]">
+                  <h1 className="text-xl font-bold">{item.title}</h1>
+                </div>
+                <div>{item.answers}</div>
+              </div>
+              <div className="mt-2 mb-2 text-sm text-gray-500">
+                <p>{item.description}</p>
+              </div>
+              <div className="flex flex-row justify-between mt-2">
+                <div className="w-full">
+                  <span className="bg-green-500 text-white rounded-md p-1 text-sm mr-2">
+                    {item.category[0].category1}
+                  </span>
+                  <span className="bg-green-500 text-white rounded-md p-1 text-sm mr-2">
+                    {item.category[0].category2}
+                  </span>
+                </div>
+                <div className="flex flex-row">
+                  <div className="w-[25px] h-[25px] mr-1">
+                    <img src="/img/avatar.png" alt="avatar icon" />
+                  </div>
+                  <div className="text-green-500">{item.username}</div>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>

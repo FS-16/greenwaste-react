63a33088d8ed3fcdd328daf9abf373dd814ccbf7
Eka Riyanto <ekariyanto50@gmail.com>
2023-11-20 15:47:30 +0700

    feat(login): create login page

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,27 @@ import Forum from './pages/Forum';
 import NotFound from './pages/NotFound';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div>
-      <Navbar />
       <Routes>
-        <Route index element={<Home />} />
-        <Route path="/solution" element={<Solution />} />
-        <Route path="/forum" element={<Forum />} />
+      <Route
+          path="/"
+          element={
+            <div>
+              <Navbar />
+              <Route index element={<Home />} />
+              <Route path="/solution" element={<Solution />} />
+              <Route path="/forum" element={<Forum />} />
+              <Footer />
+            </div>
+          }
+        />
+        <Route path="/login" element={<Login />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-      <Footer />
     </div>
   );
 }

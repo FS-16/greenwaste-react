fe92872aad725887c3150e91390ec2c6039a0fb2
koreoxy <rayssankn@gmail.com>
2023-11-20 22:39:42 +0700

    fix: routes link navbar

---
+++ src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import handleTitle from '../handle/handleTitle';
+
+function Login() {
+  handleTitle('Login | GreenWaste');
+
+  return <div>Login</div>;
+}
+
+export default Login;
---
+++ src/pages/Register.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import handleTitle from '../handle/handleTitle';
+
+function Register() {
+  handleTitle('Register | GreenWaste');
+
+  return <div>Register</div>;
+}
+
+export default Register;

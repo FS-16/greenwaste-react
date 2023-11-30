70864c19c7c78ac6dffc4298f7634f7755f28504
koreoxy <rayssankn@gmail.com>
2023-11-30 12:44:22 +0700

    fix: login and register pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,56 +1,122 @@
-import { useState } from "react"
+import { useState } from 'react';
 import handleTitle from '../handle/handleTitle';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  loginStart,
+  loginSuccess,
+  loginFailure,
+} from '../redux/user/userSlice';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
-    handleTitle('Login | GreenWaste');
+  handleTitle('Login | GreenWaste');
+  const [formData, setFormData] = useState({});
+  const { loading, error } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
 
-    const handleEmailChange = (e) => {
-        setEmail(e.target.value)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(loginStart());
+      const res = await fetch('/api/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(loginFailure(data.message));
+        return;
+      }
+      dispatch(loginSuccess(data));
+
+      // if login succesful and navigate to home page
+      navigate('/');
+    } catch (error) {
+      dispatch(loginFailure(error.message));
     }
-    
-    const handlePasswordChange = (e) => {
-        setPassword(e.target.value)
-    }
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-
-        console.log('Email:', email);
-        console.log('Password:', password);
-    }
-
-    return (
-      <div className="h-screen md:flex md:gap-x-3.5">
-          <img src="/img/login.jpg" className="md:w-1/2 object-cover hidden md:block" alt="login" />
-          <div className="md:w-1/2 py-4 flex flex-col justify-center items-center relative">
-            <a href="/">
-              <img src="/img/logo.png" className="absolute top-5 left-0" alt="logo" />
-            </a>
-            <div className="">
-                <h1 className="text-center text-green-light text-4xl font-medium">Login to your account</h1>
-                <form className="w-fit flex flex-col gap-4 p-5 mt-20" onSubmit={handleSubmit}>
-                    <div className="form-group flex flex-col">
-                        <label htmlFor="email">Email</label>
-                        <input type="email" name="email" id="email" className="px-3 py-2 border-gray-color border-2 rounded-md" placeholder="Your Email" value={email} onChange={handleEmailChange} />
-                    </div>
-                    <div className="form-group flex flex-col">
-                        <label htmlFor="password">Password</label>
-                        <input type="password" name="password" id="password" className="px-3 py-2 border-gray-color border-2 rounded-md" placeholder="Your Password" value={password} onChange={handlePasswordChange} />
-                    </div>
-                    <p><span className="font-bold">By signing up yout to agree to out</span> Terms & Condition and privacy Policy*</p>
-                    <button type="submit" className="p-3.5 text-white bg-green-button rounded-lg">Login</button>
-                </form>
-                <div className="flex gap-1 justify-center">
-                    <p className="my-auto">Don&apos;t have an account?</p>
-                    <a href="/register" className="my-auto text-green-dark">Register</a>
-                </div>
-              </div>
+  return (
+    <div className="h-screen md:flex md:gap-x-3.5">
+      <img
+        src="/img/login.jpg"
+        className="md:w-1/2 object-cover hidden md:block"
+        alt="login"
+      />
+      <div className="md:w-1/2 py-4 flex flex-col justify-center items-center relative">
+        <a href="/">
+          <img
+            src="/img/logo.png"
+            className="absolute top-5 left-0"
+            alt="logo"
+          />
+        </a>
+        <div className="mt-20 lg:mt-5">
+          <h1 className="text-center text-green-light text-4xl font-medium mt-5">
+            Login to your account
+          </h1>
+          {/* FORM LOGIN */}
+          <form
+            className="w-fit flex flex-col gap-4 p-5"
+            onSubmit={handleSubmit}
+          >
+            <div className="form-group flex flex-col">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className="px-3 py-2 border-gray-color border-2 rounded-md"
+                placeholder="Your Email"
+                onChange={handleChange}
+              />
             </div>
+            <div className="form-group flex flex-col">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                className="px-3 py-2 border-gray-color border-2 rounded-md"
+                placeholder="Your Password"
+                onChange={handleChange}
+              />
+            </div>
+            <p>
+              <span className="font-bold">
+                By signing up yout to agree to out
+              </span>{' '}
+              Terms & Condition and privacy Policy*
+            </p>
+            <button
+              disabled={loading}
+              className="p-3.5 text-white bg-green-button hover:bg-green-800 rounded-lg hover:opacity-95 disabled:opacity-80"
+            >
+              {loading ? 'Loading...' : 'Login'}
+            </button>
+          </form>
+          <div className="flex gap-1 justify-center">
+            <p className="my-auto">Don&apos;t have an account?</p>
+            <a href="/register" className="my-auto text-green-dark">
+              Register
+            </a>
+          </div>
         </div>
-    )
-  }
-  
-  export default Login
+        {error && <p className="text-red-500 mt-5">{error}</p>}
+      </div>
+    </div>
+  );
+}
+
+export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,66 +1,130 @@
-import { useState } from "react"
+import { useState } from 'react';
 import handleTitle from '../handle/handleTitle';
+import { useNavigate } from 'react-router-dom';
 
 function Register() {
-    handleTitle('Register | GreenWaste');
+  handleTitle('Register | GreenWaste');
 
-    const [username, setUsername] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-    const handleUsernameChange = (e) => {
-        setUsername(e.target.value)
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch('/api/auth/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/login');
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
     }
-    
-    const handleEmailChange = (e) => {
-        setEmail(e.target.value)
-    }
-    
-    const handlePasswordChange = (e) => {
-        setPassword(e.target.value)
-    }
+  };
 
-    const handleSubmitRegister = async (e) => {
-        e.preventDefault()
+  return (
+    <div className="h-screen md:flex md:flex-row-reverse md:gap-x-3.5">
+      <img
+        src="/img/register.jpg"
+        className="md:w-1/2 object-cover hidden md:block"
+        alt="login"
+      />
+      <div className="md:w-1/2 py-4 flex flex-col justify-center items-center relative">
+        <a href="/">
+          <img
+            src="/img/logo.png"
+            className="absolute top-5 left-0"
+            alt="logo"
+          />
+        </a>
+        <div className="mt-20 lg:mt-5">
+          <h1 className="text-center text-green-light text-4xl font-medium mt-5">
+            Create an account
+          </h1>
 
-        console.log('Username:', username);
-        console.log('Email:', email);
-        console.log('Password:', password);
-    }
-
-    return (
-        <div className="h-screen md:flex md:flex-row-reverse md:gap-x-3.5">
-            <img src="/img/register.jpg" className="md:w-1/2 object-cover hidden md:block" alt="login" />
-            <div className="md:w-1/2 py-4 flex flex-col justify-center items-center relative">
-                <a href="/">
-                    <img src="/img/logo.png" className="absolute top-5 left-0" alt="logo" />
-                </a>
-                <div className="">
-                    <h1 className="text-center text-green-light text-4xl font-medium">Create an account</h1>
-                    <form className="w-fit flex flex-col gap-4 p-5 mt-20" onSubmit={handleSubmitRegister}>
-                        <div className="form-group flex flex-col">
-                            <label htmlFor="username">Username</label>
-                            <input type="username" name="username" id="username" className="px-3 py-2 border-gray-color border-2 rounded-md" placeholder="Your Username" value={username} onChange={handleUsernameChange} />
-                        </div>
-                        <div className="form-group flex flex-col">
-                            <label htmlFor="email">Email</label>
-                            <input type="email" name="email" id="email" className="px-3 py-2 border-gray-color border-2 rounded-md" placeholder="Your Email" value={email} onChange={handleEmailChange} />
-                        </div>
-                        <div className="form-group flex flex-col">
-                            <label htmlFor="password">Password</label>
-                            <input type="password" name="password" id="password" className="px-3 py-2 border-gray-color border-2 rounded-md" placeholder="Your Password" value={password} onChange={handlePasswordChange} />
-                        </div>
-                        <p className="text-sm"><span className="font-bold">By signing up yout to agree to out</span> Terms & Condition and privacy Policy*</p>
-                        <button type="submit" className="p-3.5 text-white bg-green-button rounded-lg">Register</button>
-                    </form>
-                    <div className="flex gap-1 justify-center text-sm">
-                        <p className="my-auto">Already signed up?</p>
-                        <a href="/login" className="my-auto text-green-dark">Login</a>
-                    </div>
-                </div>
+          {/* FORM REGISTER */}
+          <form
+            className="w-fit flex flex-col gap-4 p-5"
+            onSubmit={handleSubmit}
+          >
+            <div className="form-group flex flex-col">
+              <label htmlFor="username">Username</label>
+              <input
+                type="username"
+                name="username"
+                id="username"
+                className="px-3 py-2 border-gray-color border-2 rounded-md"
+                placeholder="Your Username"
+                onChange={handleChange}
+              />
             </div>
+            <div className="form-group flex flex-col">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className="px-3 py-2 border-gray-color border-2 rounded-md"
+                placeholder="Your Email"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form-group flex flex-col">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                className="px-3 py-2 border-gray-color border-2 rounded-md"
+                placeholder="Your Password"
+                onChange={handleChange}
+              />
+            </div>
+            <p className="text-sm">
+              <span className="font-bold">
+                By signing up yout to agree to out
+              </span>{' '}
+              Terms & Condition and privacy Policy*
+            </p>
+            <button
+              disabled={loading}
+              className="p-3.5 text-white bg-green-button hover:bg-green-800 rounded-lg hover:opacity-95 disabled:opacity-80"
+            >
+              {loading ? 'Loading...' : 'Register'}
+            </button>
+          </form>
+          <div className="flex gap-1 justify-center text-sm">
+            <p className="my-auto">Already signed up?</p>
+            <a href="/login" className="my-auto text-green-dark">
+              Login
+            </a>
+          </div>
         </div>
-    )
+        {error && <p className="text-red-500 mt-5">{error}</p>}
+      </div>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
